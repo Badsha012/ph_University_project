6969a8e3a5ead528ce71bd666b2e34b5222b9551
Badsha012 <badshasheikh361@gmail.com>
2025-09-24 10:11:05 +0600

    nine commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {} from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
+import Container from './Components/Container'
 
 function App() {
   
@@ -10,11 +11,27 @@ function App() {
   return (
     <div>
     <Navbar></Navbar>
-    <h1 className='text-red-800'>Root File</h1>
-    <Footer></Footer>
-     
-      
-     
+    <Container>
+         <div className='grid md:grid-cols-3 grid-cols-1 gap-[20px] my-[50px]'>
+      <div className='rounded-md p-7 text-white h-[250px] bg-gray-600 flex flex-col items-center'>
+        <h1 className='font-bold text-[35px]' >Pending</h1>
+        <p className='font-semibold text-[26px]'>0</p>
+
+      </div>
+        <div className='rounded-md p-7 text-white h-[250px] bg-purple-600 flex flex-col items-center'>
+               <h1 className='font-bold text-[35px]' >Submitted</h1>
+        <p className='font-semibold text-[26px]'>0</p>
+
+      </div>
+        <div className='rounded-md p-7 text-white h-[250px] bg-teal-600 flex flex-col items-center'>
+               <h1 className='font-bold text-[35px]' >Reviewed</h1>
+        <p className='font-semibold text-[26px]'>0</p>
+
+      </div>
+
+    </div>
+    </Container>
+    <Footer></Footer> 
     </div>
   )
 }
