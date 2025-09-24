6b415faf319ebbdd9cef1070827bd9358fcd99cf
Badsha012 <badshasheikh361@gmail.com>
2025-09-24 09:47:07 +0600

    nine commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import {} from 'react'
 
 import './App.css'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 function App() {
   
 
   return (
-    <>
+    <div>
+    <Navbar></Navbar>
     <h1 className='text-red-800'>Root File</h1>
+    <Footer></Footer>
      
       
      
-    </>
+    </div>
   )
 }
 
